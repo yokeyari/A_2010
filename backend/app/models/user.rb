fe0fc232bd5f7f3982e844f7b270e43e1bbf8050
Kumagai <koshiro.kumagai.r7@dc.tohoku.ac.jp>
2020-11-04 17:20:06 +0900

    bug fix

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :pages
+  has_many :pages, dependet: :destroy
   validates :name, presence: true, length: {maximum: 100}
   validates :email, presence: true, uniqueness: true, format: {with: //}
 end
