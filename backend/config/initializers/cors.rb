7021bb4a20237a8546be00bf0658ff50562030c2
Kumagai <koshiro.kumagai.r7@dc.tohoku.ac.jp>
2020-11-04 21:40:09 +0900

    bug fix

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -5,6 +5,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
       resource "*",
         headers: :any,
-        methods: [:get, :post, :options, :head, :delete]
+        methods: [:get, :post, :patch,:options, :head, :delete]
     end
   end
