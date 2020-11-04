b162fc35b9d132b7355b4623b56126a4a859b32b
tks3141 <28818747+tks3141@users.noreply.github.com>
2020-11-05 00:46:47 +0900

    merge miss user

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_save {email.downcase!}
+  has_secure_password
   has_many :pages, dependent: :destroy
   validates :name, presence: true, length: {maximum: 100}
   validates :email, presence: true, uniqueness: {case_sensitive: false}, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
