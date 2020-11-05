697b890a10a8299bbc65a0f8497a1ca7141d5d07
tks3141 <28818747+tks3141@users.noreply.github.com>
2020-11-05 18:18:25 +0900

    memo の配置直し

--- front/src/Main/Memo/MemoComponent.js
+++ front/src/Main/Memo/MemoComponent.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { makeStyles } from '@material-ui/core/styles';
-import {formatSec2Str} from '../Duration';
+import { formatSec2Str } from '../Duration';
 import Card from '@material-ui/core/Card'
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
@@ -26,44 +26,44 @@ const useStyles = makeStyles((theme) => ({
   },
 }
 ));
-function MemoComponent(props){
+function MemoComponent(props) {
   const classes = useStyles();
   const [isEditMode, setEditMode] = useState(false);
-  const [text,setText] = useState(props.memo.text)
+  const [text, setText] = useState(props.memo.text)
   const memo = props.memo
   const player = props.player
-  
-  const endEditMode = ()=>{
-    props.onChange({...memo,text:text});
+
+  const endEditMode = () => {
+    props.onChange({ ...memo, text: text });
     setEditMode(false);
   }
 
-  const handleJump = ()=>{
+  const handleJump = () => {
     player.player.player.seekTo(memo.time)
   }
 
 
-  const body = 
+  const body =
     isEditMode ?
-      <TextField           
-      multiline
-      type="text" id="memo-input" className="" onChange={(e) => setText(e.target.value)} value={text}/>
-    :
+      <TextField
+        multiline
+        type="text" id="memo-input" className="" onChange={(e) => setText(e.target.value)} value={text} />
+      :
       memo.text;
   return (
     //<div className="Post-memos" key={memo.id}>
     <Card className={classes.card} key={memo.id}>
       <CardActions>
-      <Button className="timeButton" startIcon={<AccessTimeIcon />}onClick={()=>{handleJump()}} >{formatSec2Str(memo.time)}</Button>
-      <DescriptionIcon />{body}
+        <Button className="timeButton" startIcon={<AccessTimeIcon />} onClick={() => { handleJump() }} >{formatSec2Str(memo.time)}</Button>
+        {isEditMode ?
+          <Button className="edit" startIcon={<DoneIcon />} onClick={() => { endEditMode() }}>done</Button> :
+          <Button className="edit" color="primary" startIcon={<EditIcon />} onClick={() => { setEditMode(true) }}>edit</Button>}
+        <Button className="delete" color="secondary" startIcon={<DeleteIcon />} onClick={() => { props.onDelete(memo) }}>delete</Button>
       </CardActions>
       <CardActions>
-      {isEditMode ? 
-        <Button className="edit" startIcon={<DoneIcon />}onClick={() => {endEditMode()} }>done</Button>:
-        <Button className="edit" color="primary" startIcon={<EditIcon />} onClick={() => { setEditMode(true) }}>edit</Button>}
-      <Button className="delete" color="secondary" startIcon={<DeleteIcon />} onClick={() => { props.onDelete(memo) }}>delete</Button>
+        <DescriptionIcon />{body}
       </CardActions>
-      </Card>
+    </Card>
     //</div>
   )
 }
