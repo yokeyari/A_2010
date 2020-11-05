80e03d2bc8850bf31512ce004abdc4c8d6601feb
Kumagai <koshiro.kumagai.r7@dc.tohoku.ac.jp>
2020-11-05 17:30:02 +0900

    big fix

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
       resource "*",
         headers: :any,
-        methods: [:get, :post, :patch, :options, :head, :delete]
+        methods: [:get, :post, :patch, :options, :head, :delete],
         credentials: true
     end
   end
