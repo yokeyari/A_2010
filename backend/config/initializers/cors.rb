0245c036fec9580e5f242b4e1c516bcc97125806
Kumagai <koshiro.kumagai.r7@dc.tohoku.ac.jp>
2020-11-05 17:21:57 +0900

    bug fix

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,10 +1,10 @@
 # Permit cross origin
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://localhost:3000'
+      origins 'http://localhost:3000'
 
       resource "*",
         headers: :any,
-        methods: [:get, :post, :patch,:options, :head, :delete]
+        methods: [:get, :post, :patch, :options, :head, :delete]
     end
   end
