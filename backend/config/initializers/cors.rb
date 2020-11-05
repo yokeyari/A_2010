2dde3a5624746d1f1e9fe3284abc4985825fda04
Kumagai <koshiro.kumagai.r7@dc.tohoku.ac.jp>
2020-11-05 19:46:36 +0900

    bug fix

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,7 +1,16 @@
 # Permit cross origin
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'localhost:3000', 'http://memotube.xyz'
+      origins 'localhost:3000'
+
+      resource "*",
+        headers: :any,
+        methods: [:get, :post, :patch, :options, :head, :delete],
+        credentials: true
+    end
+
+    allow do
+      origins 'http://memotube.xyz:3000'
 
       resource "*",
         headers: :any,
