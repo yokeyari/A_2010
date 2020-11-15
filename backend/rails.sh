adc1c9302a5b87140cacf9bfd518724f22c033da
tks3141 <28818747+tks3141@users.noreply.github.com>
2020-11-16 06:32:39 +0900

    add railssh

--- backend/rails.sh
+++ backend/rails.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-ssh -t yokeyari@192.168.0.105 "source /etc/profile;docker-compose -f ~/memotube/A_2010/backend/docker-compose.yml exec web bash"
+ssh -t yokeyari@memotube.xyz "source /etc/profile;docker-compose -f ~/memotube/A_2010/backend/docker-compose.yml exec web bash"
 exit 0
