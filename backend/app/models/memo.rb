368f8d0d8dee3edde34cc7ff8a3769af67de5ad1
Kumagai <koshiro.kumagai.r7@dc.tohoku.ac.jp>
2020-11-18 15:00:25 +0900

    hai

--- backend/app/models/memo.rb
+++ backend/app/models/memo.rb
@@ -3,9 +3,5 @@ class Memo < ApplicationRecord
   belongs_to :user
   validates :text, presence: :true, length: {maximum: 100}
   validates :time, presence: :true
-<<<<<<< HEAD
-  enum :status, {pli: 0, pub: 1}
-=======
-  enum status: {plivate: 0, public: 1}
->>>>>>> 903cd360bafbc11edde37c1773db96bcb30b093e
+  enum status: {pli: 0, pub: 1}
 end
