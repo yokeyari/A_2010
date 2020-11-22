58a5312f3aa1108a1cded05cc1357dba35aba786
Kumagai <koshiro.kumagai.r7@dc.tohoku.ac.jp>
2020-11-22 14:15:20 +0900

    bug fix

--- backend/app/controllers/concerns/my_exception.rb
+++ backend/app/controllers/concerns/my_exception.rb
@@ -1,6 +1,6 @@
 module MyException
   class MyForbidden < Exception; end
-  class MyUnauthorized < Exceptio; end
+  class MyUnauthorized < Exception; end
 
   rescue_from ActiveRecord::RecordNotFound, with: :res_not_found
   rescue_from ActiveRecord::RecordInvalid,  with: :res_errors_record
