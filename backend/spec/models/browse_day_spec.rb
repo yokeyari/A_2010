4eb963292c09c3deaf9cbf182e7f16bdc7f68562
Kumagai <koshiro.kumagai.r7@dc.tohoku.ac.jp>
2020-11-23 19:01:38 +0900

    change column day

--- backend/spec/models/browse_day_spec.rb
+++ backend/spec/models/browse_day_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe BrowseDay, type: :model do
   end
 
   it 'is valid with user_id, page_id and day' do
-    browse_day = BrowseDay.new(user_id: @user.id, page_id: @page.id, day: 1)
+    browse_day = BrowseDay.new(user_id: @user.id, page_id: @page.id, day: Time.now)
     expect(browse_day).to be_valid
   end
 
@@ -21,14 +21,15 @@ RSpec.describe BrowseDay, type: :model do
   end
 
   it 'dose not allow same [user_id, page_id, day]' do
-    BrowseDay.create(user_id: @user.id, page_id: @page.id, day: 1)
-    browse_day = BrowseDay.new(user_id: @user.id, page_id: @page.id, day: 1)
+    now = Time.now
+    BrowseDay.create(user_id: @user.id, page_id: @page.id, day: now)
+    browse_day = BrowseDay.new(user_id: @user.id, page_id: @page.id, day: now)
     expect(browse_day).not_to be_valid
   end
 
   it 'is valid different day' do
-    BrowseDay.create(user_id: @user.id, page_id: @page.id, day: 1)
-    browse_day = BrowseDay.new(user_id: @user.id, page_id: @page.id, day: 2)
+    BrowseDay.create(user_id: @user.id, page_id: @page.id, day: Time.now)
+    browse_day = BrowseDay.new(user_id: @user.id, page_id: @page.id, day: Time.now)
     expect(browse_day).to be_valid
   end
 end
