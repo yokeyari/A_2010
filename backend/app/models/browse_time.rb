921760a8300842a83a152c006c8086d70bfbf6c3
Kumagai <koshiro.kumagai.r7@dc.tohoku.ac.jp>
2020-11-23 18:13:02 +0900

    model test add

--- backend/app/models/browse_time.rb
+++ backend/app/models/browse_time.rb
@@ -1,4 +1,6 @@
 class BrowseTime < ApplicationRecord
   belongs_to :user
   belongs_to :page
+
+  validates :time, presence: true, uniqueness: {scope: [:user_id, :page_id]}
 end
--- backend/spec/models/browse_time_spec.rb
+++ backend/spec/models/browse_time_spec.rb
@@ -7,11 +7,28 @@ RSpec.describe BrowseTime, type: :model do
   end
 
   it 'is valid with user_id, page_id and time' do
-    browse_time = BrowseTime.new(
-      user_id: @user.id,
-      page_id: @page.id,
-      time: 1
-    )
+    browse_time = BrowseTime.new(user_id: @user.id, page_id: @page.id, time: 1)
+    expect(browse_time).to be_valid
+  end
+
+  it 'is invalid without user_id, page_id or time' do
+    browse_time = BrowseTime.new(user_id: nil, page_id: nil, time: nil)
+    browse_time.valid?
+    
+    expect(browse_time.errors[:user]).to include("must exist")
+    expect(browse_time.errors[:page]).to include("must exist")
+    expect(browse_time.errors[:time]).to include("can't be blank")
+  end
+
+  it 'dose not allow same [user_id, page_id, time]' do
+    BrowseTime.create(user_id: @user.id, page_id: @page.id, time: 1)
+    browse_time = BrowseTime.new(user_id: @user.id, page_id: @page.id, time: 1)
+    expect(browse_time).not_to be_valid
+  end
+
+  it 'is valid different time' do
+    BrowseTime.create(user_id: @user.id, page_id: @page.id, time: 1)
+    browse_time = BrowseTime.new(user_id: @user.id, page_id: @page.id, time: 2)
     expect(browse_time).to be_valid
   end
 end
