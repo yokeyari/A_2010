a83d74d0d2a085026d9b7a52be2c4ae0f5274294
yashima ryota <fukuoka19_1@icloud.com>
2020-11-24 04:44:17 +0900

    cosutom browse

--- front/src/Analytics/Analytics.js
+++ front/src/Analytics/Analytics.js
@@ -4,48 +4,158 @@ import React, { useState, useEffect } from 'react';
 
 export default function Analytics(props) {
 
+  const states = ["total_play", "total_write_memo", "total_else"]
   // テスト用
-  const demoX = Array(100).fill(0).map((_, i) => (i + 1) / 10);
-  const demoY = [demoX.map(v => Math.sin(v)), demoX.map(v => Math.cos(v))]
+  const demoX = {"float number": Array(100).fill(0).map((_, i) => (i + 1) / 10)};
+  const demoY = {"sin": Object.values(demoX)[0].map(v => Math.sin(v)), "cos": Object.values(demoX)[0].map(v => Math.cos(v))}
+  const demoData = {X: demoX, Y_list: demoY} 
 
-  const [data, setData] = useState(null)
-  
-  const data_list = {
-    demo: {
-      labels: ["float number", "output"], 
-      categories: ["sin", "cos"], 
-      colors: ["#8884d8", "#82ca9d"], 
-      X: demoX, 
-      Y_list: demoY
+  const [dataList, setDataList] = useState(
+    {
+      // demo: demoData,
+      browse_times: null,
+      browse_dates: null
     }
+  )
+  const [data, setData] = useState({X: {"": []}, Y_list: {"  ( )": []}});
+  const [userIds, setUserIds] = useState([]);
+  const [visState, setVisState] = useState("all");
+  const [visUser, setVisUser] = useState("all");
+  // const [body, setBody] = useState(null);
+
+
+  function createData(table, xlabel) {
+    const user_ids = [...new Set(table.map(record => record.user_id))];
+    let X = [...new Set(table.map(record => record[xlabel]))].sort();
+    const Y_all = Object.fromEntries(
+      Array.prototype.concat.apply([], 
+        states.map(state => 
+          user_ids.map(user_id => 
+            [
+              state+` (${user_id})`, 
+              X.map( x => {
+                const value = table.find( record =>  (record.user_id==user_id && record[xlabel]==x) )
+                return value ? value[state] : 0
+              })
+            ]
+          )
+        )
+      )
+    )
+    X = {[xlabel]: X};
+    return [user_ids, {X: X, Y_list: Y_all}]
   }
+  
 
   useEffect(() => {
-    setData(data_list["demo"]);
-    console.log(data);
+    // get analytics
+    const browseTimes = [
+      {user_id: 1, time: 1, total_play: 5, total_write_memo: 3, total_else: 0},
+      {user_id: 1, time: 2, total_play: 4, total_write_memo: 3, total_else: 0},
+      {user_id: 1, time: 3, total_play: 3, total_write_memo: 3, total_else: 0},
+      {user_id: 1, time: 4, total_play: 2, total_write_memo: 3, total_else: 0},
+      {user_id: 1, time: 5, total_play: 1, total_write_memo: 3, total_else: 0},
+      {user_id: 2, time: 1, total_play: 5, total_write_memo: 3, total_else: 0},
+      {user_id: 2, time: 2, total_play: 4, total_write_memo: 3, total_else: 0},
+      // {user_id: 2, time: 3, total_play: 3, total_write_memo: 3, total_else: 0},
+      {user_id: 2, time: 4, total_play: 2, total_write_memo: 3, total_else: 0},
+      {user_id: 2, time: 5, total_play: 1, total_write_memo: 3, total_else: 0},
+    ]
+    const browseDays = [
+      {user_id: 1, day: 1, total_play: 5, total_write_memo: 3, total_else: 0},
+      {user_id: 1, day: 2, total_play: 4, total_write_memo: 3, total_else: 0},
+      {user_id: 1, day: 3, total_play: 3, total_write_memo: 3, total_else: 0},
+      {user_id: 1, day: 4, total_play: 2, total_write_memo: 3, total_else: 0},
+      {user_id: 1, day: 5, total_play: 1, total_write_memo: 3, total_else: 0},
+      {user_id: 2, day: 1, total_play: 5, total_write_memo: 3, total_else: 0},
+      {user_id: 2, day: 2, total_play: 4, total_write_memo: 3, total_else: 0},
+      {user_id: 2, day: 3, total_play: 3, total_write_memo: 3, total_else: 0},
+      {user_id: 2, day: 4, total_play: 2, total_write_memo: 3, total_else: 0},
+      {user_id: 2, day: 5, total_play: 1, total_write_memo: 3, total_else: 0},
+    ]
+
+    const [user_ids, data_time] = createData(browseTimes, "time");
+    const [_, data_day] = createData(browseDays, "day");
+
+    setDataList({...dataList, browse_times: data_time, browse_dates: data_day});
+    setUserIds(user_ids);
+    setData(dataList["browse_times"]);
+    // console.log(data)
+    // console.log(dataList)
+    // console.log(data_time)
   }, [])
+
+  // useEffect(() => {
+  //   data ? setBody( <Graph2D X={data.X} Y_list={data.Y_list} vis_state={visState} /> ) : setBody(null)
+  // }, [data, visState])
 
   const handleChangeGraph = (event) => {
     const graph_name = event.target.value;
-    setData(data_list[graph_name]);
+    console.log(graph_name);
+    setData(dataList[graph_name]);
+  }
+
+  const handleChangeState = (event) => {
+    const state = event.target.value;
+    setVisState(state);
+  }
+
+  const handleChangeUser = (event) => {
+    const vis_user = event.target.value
+    setVisUser(vis_user);
   }
 
   return (
     <div>
-      <h1>グラフテスト</h1>
+      <h1>動画分析</h1>
+      <Grid container direction="row" justify="center" alignItems="center">
       <Grid item>
         <FormControl>
           <InputLabel id="demo-simple-select-label">graph type</InputLabel>
           <Select onChange={handleChangeGraph}
-            defaultValue={"demo"}
+            defaultValue={"none"}
             className={""}
             inputProps={{ "aria-label": "Without label" }}
           >
-            <MenuItem value={"demo"} >demo graph</MenuItem>
+            <MenuItem value={"none"} key={0} >none</MenuItem>
+            <MenuItem value={"browse_times"} key={1} >browse time</MenuItem>
+            <MenuItem value={"browse_dates"} key={2} >browse date</MenuItem>
           </Select>
         </FormControl>
       </Grid>
-      {data ? <Graph2D X={data.X} Y_list={data.Y_list} labels={data.labels} categories={data.categories} colors={data.colors} /> : null}
+      <Grid item>
+        <FormControl>
+          <InputLabel id="demo-simple-select-label">state</InputLabel>
+          <Select onChange={handleChangeState}
+            defaultValue={"all"}
+            className={""}
+            inputProps={{ "aria-label": "Without label" }}
+          >
+            <MenuItem value={"all"} key={0} >all</MenuItem>
+            <MenuItem value={"total_play"} key={1} >total play</MenuItem>
+            <MenuItem value={"total_write_memo"} key={2} >total write memo</MenuItem>
+            <MenuItem value={"total_else"} key={3} >total else</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
+      <Grid item>
+        <FormControl>
+          <InputLabel id="demo-simple-select-label">user_id</InputLabel>
+          <Select onChange={handleChangeUser}
+            defaultValue={"all"}
+            className={""}
+            inputProps={{ "aria-label": "Without label" }}
+          >
+            <MenuItem value={"all"} key={0} >all</MenuItem>
+            {
+              userIds.map(user_id => <MenuItem value={user_id} key={user_id} >{user_id}</MenuItem> )
+            }
+          </Select>
+        </FormControl>
+      </Grid>
+      </Grid>
+      { data ? <Graph2D X={data.X} Y_list={data.Y_list} vis_state={visState} vis_user={visUser} /> : null }
+      {/* {body} */}
     </div>
   )
 }
--- front/src/Analytics/Graph2D.js
+++ front/src/Analytics/Graph2D.js
@@ -5,12 +5,66 @@ import React, { useEffect, useState } from 'react';
 
 export default function Graph2D(props) {
 
-  const labels = props.labels;
-  const categories = props.categories;
-  const colors = props.colors;
+  const vis_state = props.vis_state;
+  const vis_user = props.vis_user;
   const X = props.X;
-  const Y_list = props.Y_list;
-  const data = X.map((v, i) => { return Object.assign({ name: v, r: 8 }, Object.fromEntries(categories.map((category, j) => [category, Y_list[j][i]]))) });
+  const Y_list = Object.entries( props.Y_list ).filter(Y => {
+    console.log(Y)
+    const state = Y[0].split(" ")[0]
+    const user_id = Y[0].split("(")[1].split(")")[0]
+    return ((vis_state=="all" || state==vis_state) && (vis_user=="all" || user_id==vis_user))
+  });
+  console.log(Y_list)
+  const data = Object.values(X)[0].map( (v, i) => 
+    Object.assign( 
+      { name: v, r: 8 }, 
+      Object.fromEntries( Y_list.map( Y =>  [Y[0], Y[1][i]] )) 
+    )
+  );
+  console.log(data)
+  const colors = [
+    "#63b598", "#ce7d78", "#ea9e70", "#a48a9e", "#c6e1e8", "#648177" ,"#0d5ac1" ,
+    "#f205e6" ,"#1c0365" ,"#14a9ad" ,"#4ca2f9" ,"#a4e43f" ,"#d298e2" ,"#6119d0",
+    "#d2737d" ,"#c0a43c" ,"#f2510e" ,"#651be6" ,"#79806e" ,"#61da5e" ,"#cd2f00" ,
+    "#9348af" ,"#01ac53" ,"#c5a4fb" ,"#996635","#b11573" ,"#4bb473" ,"#75d89e" ,
+    "#2f3f94" ,"#2f7b99" ,"#da967d" ,"#34891f" ,"#b0d87b" ,"#ca4751" ,"#7e50a8" ,
+    "#c4d647" ,"#e0eeb8" ,"#11dec1" ,"#289812" ,"#566ca0" ,"#ffdbe1" ,"#2f1179" ,
+    "#935b6d" ,"#916988" ,"#513d98" ,"#aead3a", "#9e6d71", "#4b5bdc", "#0cd36d",
+    "#250662", "#cb5bea", "#228916", "#ac3e1b", "#df514a", "#539397", "#880977",
+    "#f697c1", "#ba96ce", "#679c9d", "#c6c42c", "#5d2c52", "#48b41b", "#e1cf3b",
+    "#5be4f0", "#57c4d8", "#a4d17a", "#225b8", "#be608b", "#96b00c", "#088baf",
+    "#f158bf", "#e145ba", "#ee91e3", "#05d371", "#5426e0", "#4834d0", "#802234",
+    "#6749e8", "#0971f0", "#8fb413", "#b2b4f0", "#c3c89d", "#c9a941", "#41d158",
+    "#fb21a3", "#51aed9", "#5bb32d", "#807fb", "#21538e", "#89d534", "#d36647",
+    "#7fb411", "#0023b8", "#3b8c2a", "#986b53", "#f50422", "#983f7a", "#ea24a3",
+    "#79352c", "#521250", "#c79ed2", "#d6dd92", "#e33e52", "#b2be57", "#fa06ec",
+    "#1bb699", "#6b2e5f", "#64820f", "#1c271", "#21538e", "#89d534", "#d36647",
+    "#7fb411", "#0023b8", "#3b8c2a", "#986b53", "#f50422", "#983f7a", "#ea24a3",
+    "#79352c", "#521250", "#c79ed2", "#d6dd92", "#e33e52", "#b2be57", "#fa06ec",
+    "#1bb699", "#6b2e5f", "#64820f", "#1c271", "#9cb64a", "#996c48", "#9ab9b7",
+    "#06e052", "#e3a481", "#0eb621", "#fc458e", "#b2db15", "#aa226d", "#792ed8",
+    "#73872a", "#520d3a", "#cefcb8", "#a5b3d9", "#7d1d85", "#c4fd57", "#f1ae16",
+    "#8fe22a", "#ef6e3c", "#243eeb", "#1dc18", "#dd93fd", "#3f8473", "#e7dbce",
+    "#421f79", "#7a3d93", "#635f6d", "#93f2d7", "#9b5c2a", "#15b9ee", "#0f5997",
+    "#409188", "#911e20", "#1350ce", "#10e5b1", "#fff4d7", "#cb2582", "#ce00be",
+    "#32d5d6", "#17232", "#608572", "#c79bc2", "#00f87c", "#77772a", "#6995ba",
+    "#fc6b57", "#f07815", "#8fd883", "#060e27", "#96e591", "#21d52e", "#d00043",
+    "#b47162", "#1ec227", "#4f0f6f", "#1d1d58", "#947002", "#bde052", "#e08c56",
+    "#28fcfd", "#bb09b", "#36486a", "#d02e29", "#1ae6db", "#3e464c", "#a84a8f",
+    "#911e7e", "#3f16d9", "#0f525f", "#ac7c0a", "#b4c086", "#c9d730", "#30cc49",
+    "#3d6751", "#fb4c03", "#640fc1", "#62c03e", "#d3493a", "#88aa0b", "#406df9",
+    "#615af0", "#4be47", "#2a3434", "#4a543f", "#79bca0", "#a8b8d4", "#00efd4",
+    "#7ad236", "#7260d8", "#1deaa7", "#06f43a", "#823c59", "#e3d94c", "#dc1c06",
+    "#f53b2a", "#b46238", "#2dfff6", "#a82b89", "#1a8011", "#436a9f", "#1a806a",
+    "#4cf09d", "#c188a2", "#67eb4b", "#b308d3", "#fc7e41", "#af3101", "#ff065",
+    "#71b1f4", "#a2f8a5", "#e23dd0", "#d3486d", "#00f7f9", "#474893", "#3cec35",
+    "#1c65cb", "#5d1d0c", "#2d7d2a", "#ff3420", "#5cdd87", "#a259a4", "#e4ac44",
+    "#1bede6", "#8798a4", "#d7790f", "#b2c24f", "#de73c2", "#d70a9c", "#25b67",
+    "#88e9b8", "#c2b0e2", "#86e98f", "#ae90e2", "#1a806b", "#436a9e", "#0ec0ff",
+    "#f812b3", "#b17fc9", "#8d6c2f", "#d3277a", "#2ca1ae", "#9685eb", "#8a96c6",
+    "#dba2e6", "#76fc1b", "#608fa4", "#20f6ba", "#07d7f6", "#dce77a", "#77ecca"
+  ]
+
 
   const handleClickLine = (event) => {
     console.log("click event", event);
@@ -18,7 +72,7 @@ export default function Graph2D(props) {
 
   return (
     <div style={{ width: '90%', height: 500 }}>
-      <h4>{labels[1]}</h4>
+      <h4>frequency</h4>
       <ResponsiveContainer>
         <LineChart width={600} height={300} data={data}
           margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
@@ -27,14 +81,14 @@ export default function Graph2D(props) {
           <CartesianGrid strokeDasharray="3 3" />
           <Tooltip />
           <Legend />
-          {categories.map((category, i) => {
+          {Y_list.map((Y, i) => {
             return (
-              <Line type="monotone" dataKey={category} stroke={colors[i]} activeDot={{ onClick: handleClickLine, r: 8 }} />
+              <Line type="monotone" dataKey={Y[0]} stroke={colors[i]} activeDot={{ onClick: handleClickLine, r: 8 }} />
             )
           })}
         </LineChart>
       </ResponsiveContainer>
-      <h4>{labels[0]}</h4>
+      <h4>{Object.keys(X)[0]}</h4>
     </div>
   )
 }
